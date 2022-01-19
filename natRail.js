268c0e2fe41d586a58a32c052cdd7d3a498af08a
smcgann99 <52132596+smcgann99@users.noreply.github.com>
2022-01-19 17:35:34 +0000

    Update natRail.js
    Remove un-needed input validation

--- natRail.js
+++ natRail.js
@@ -1,11 +1,11 @@
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     "use strict";
     var Rail = require('national-rail-darwin')
 
     function NatRailNode(n) {
-        RED.nodes.createNode(this,n);
-        this.property = n.property||"payload";
+        RED.nodes.createNode(this, n);
+        this.property = n.property || "payload";
         this.scode = n.scode || "";
         this.destscode = n.destscode || "";
 
@@ -20,39 +20,35 @@ module.exports = function(RED) {
         var rail = new Rail(this.apikey);
         var node = this;
 
-        this.on("input", function(msg) {
-            var value = RED.util.getMessageProperty(msg,node.property);
-            if (value !== undefined) {
-                var stationcode = node.scode || msg.station;
-                var destinationstationcode = node.destscode || msg.destination_station;
-                var options = {};
-                if (destinationstationcode) {
-                    options.destination = destinationstationcode;
-                }
-                rail.getDepartureBoard(stationcode, options, function(err,result) {
-                    if (err) {
-                        if (err.statusCode === 401) {
-                            node.error("Unauthorised - Is API key correct ?",msg);
-                        }
-                        else {
-                            node.error("Error :"+err.statusCode,msg);
-                        }
+        this.on("input", function (msg) {
+            var stationcode = node.scode || msg.station;
+            var destinationstationcode = node.destscode || msg.destination_station;
+            var options = {};
+            if (destinationstationcode) {
+                options.destination = destinationstationcode;
+            }
+            rail.getDepartureBoard(stationcode, options, function (err, result) {
+                if (err) {
+                    if (err.statusCode === 401) {
+                        node.error("Unauthorised - Is API key correct ?", msg);
                     }
                     else {
-                        if (result.hasOwnProperty("trainServices")) {
-                            value = result.trainServices;
-                            RED.util.setMessageProperty(msg,node.property,value);
-                            msg.topic = stationcode;
-                            node.send(msg);
-                        }
-                        else {
-                            node.warn("Odd return from Train API call");
-                        }
+                        node.error("Error :" + err.statusCode, msg);
                     }
-                })
-            }
-            else { node.send(msg); }
+                }
+                else {
+                    if (result.hasOwnProperty("trainServices")) {
+                        let value = result.trainServices;
+                        RED.util.setMessageProperty(msg, node.property, value);
+                        msg.topic = stationcode;
+                        node.send(msg);
+                    }
+                    else {
+                        node.warn("Odd return from Train API call");
+                    }
+                }
+            })
         });
     }
-    RED.nodes.registerType("natrail",NatRailNode);
+    RED.nodes.registerType("natrail", NatRailNode);
 }
